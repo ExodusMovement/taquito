0925ebb6f2d4752b766524b0dec527d19c373dc4
Matias Lang <matias.la@exodus.io>
2023-05-04 14:46:24 -0300

    ensure hex strings have even length

--- packages/taquito-signer/src/derivation-tools/utils.ts
+++ packages/taquito-signer/src/derivation-tools/utils.ts
@@ -2,6 +2,9 @@ import { InvalidHexStringError } from '@exodus/taquito-utils';
 
 export function parseHex(s: string): Uint8Array {
   const res: number[] = [];
+  if (s.length % 2 !== 0) {
+    throw new InvalidHexStringError(`expected hex string to have even length`)
+  }
   for (let i = 0; i < s.length; i += 2) {
     const ss = s.slice(i, i + 2);
     const x = parseInt(ss, 16);
